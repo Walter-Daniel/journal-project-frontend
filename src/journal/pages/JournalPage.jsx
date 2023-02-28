a3ff55f5833fca12e2338241102cc81dec7966d4
Walter Daniel <walterdcarrizo.19@gmail.com>
2023-02-28 18:57:00 -0300

    galeria de imagenes y botón flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
@@ -8,8 +9,21 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati asperiores nisi incidunt omnis molestias itaque rem repudiandae magni suscipit similique. Nulla necessitatibus nam molestiae aspernatur eos quia, nesciunt adipisci ab.</Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined />
+      </IconButton>
 
     </JournalLayout>
   )
