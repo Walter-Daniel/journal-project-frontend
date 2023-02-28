499b6e87af4a4285cb6af8210ff532aff4891716
Walter Daniel <walterdcarrizo.19@gmail.com>
2023-02-27 23:54:01 -0300

    Layout Register

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout title='Register'>
+      <form>
+          <Grid container>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Nombre(s)" 
+                type="text" 
+                placeholder='Martín'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Apellido(s)" 
+                type="text" 
+                placeholder='Robles'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Correo" 
+                type="email" 
+                placeholder='correo@mail.com'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Contraseña" 
+                type="password" 
+                placeholder='contraseña'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid item xs={ 12 } sx={{ mt: 2 }}>
+              <TextField 
+                label="Confirmar ontraseña" 
+                type="password" 
+                placeholder='contraseña'
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+
+              <Grid item xs={ 12 }>
+                <Button variant='contained' fullWidth>
+                  Crear cuenta
+                </Button>
+              </Grid>
+
+            </Grid>
+
+            <Grid container direction='row' justifyContent='end'>
+              <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+              <Link component={ RouterLink } color='inherit' to="/auth/login">
+                Ingresar
+              </Link>
+            </Grid>
+
+          </Grid>
+        </form>
+    </AuthLayout>
   )
 }
