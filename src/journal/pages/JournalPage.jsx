ba03cc2210e3bb8fa46b9b7ad2ba351bb75d2c09
Walter Daniel <walterdcarrizo.19@gmail.com>
2023-02-28 17:57:58 -0300

    Se creó el view de cuando no hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,15 @@
 import { Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati asperiores nisi incidunt omnis molestias itaque rem repudiandae magni suscipit similique. Nulla necessitatibus nam molestiae aspernatur eos quia, nesciunt adipisci ab.</Typography>
+      {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati asperiores nisi incidunt omnis molestias itaque rem repudiandae magni suscipit similique. Nulla necessitatibus nam molestiae aspernatur eos quia, nesciunt adipisci ab.</Typography> */}
+
+      <NothingSelectedView />
+
     </JournalLayout>
   )
 }
