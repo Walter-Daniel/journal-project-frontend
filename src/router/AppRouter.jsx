7acb6d918eed95bdf5ec80620e14829c76bc92f2
Walter Daniel <walterdcarrizo.19@gmail.com>
2023-03-07 17:41:31 -0300

    Componente checking, pantalla cargando

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,18 @@
 
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import { CheckingAuth } from "../../ui/components/CheckingAuth";
 
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
 
 export const AppRouter = () => {
-  
+  const { status  } = useSelector( state => state.auth );
+
+  if( status === 'checking' ) {
+    return <CheckingAuth />
+  }
   return (
     <>
         <Routes>
