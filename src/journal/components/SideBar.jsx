4e546de6548214c756c9c09451bf3a067d4caa0b
Walter Daniel <walterdcarrizo.19@gmail.com>
2023-03-24 19:30:07 -0300

    solución al error al importar authstore en el sidebar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { useAuthStore } from "../../hooks/useAuthStore";
+import { useAuthStore } from "../../hooks/useAuthstore";
 import { Box, Divider, Drawer, List, Toolbar, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { SideBarItem } from "./SideBarItem";
